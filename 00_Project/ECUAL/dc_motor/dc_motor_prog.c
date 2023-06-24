9067039603cfc25d4a5efb82e8df17940db47640
Mahmoud Mowafey <mahmsa37@gmail.com>
2023-06-25 00:43:59 +0300

    adding all the dcm files

---
+++ 00_Project/ECUAL/dc_motor/dc_motor_prog.c
@@ -0,0 +1,16 @@
+/*
+ * DCM_Program.c
+ *
+ *     Created on: Jun 24, 2023
+ *         Author: Mahmoud Mowafey - https://github.com/Mahmoud-Mowafy
+ *  Description: This file contains all Direct Current Motor (DCM) functions' implementation.
+ */
+
+
+
+
+ /* HAL */
+#include "dcm_cfg.h"
+#include "dcm_interface.h"
+#include "pwm_interface.h"
+#include "gpt_interface.h"
