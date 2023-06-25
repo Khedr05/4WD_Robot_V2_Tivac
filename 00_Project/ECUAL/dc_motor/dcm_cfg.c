e669384a248c5803077f3b223c5958d13e436bf4
Mahmoud Mowafey <mahmsa37@gmail.com>
2023-06-25 15:35:31 +0300

    adding the configuration source file

---
+++ 00_Project/ECUAL/dc_motor/dcm_cfg.c
@@ -0,0 +1,76 @@
+/*
+ * DCM_cfg.c
+ *
+ *     Created on: Jun 25, 2023
+ *         Author: Mahmoud Mowafey - https://github.com/Mahmoud-Mowafy
+ *  Description: This file contains all Direct Current Motor (DCM) functions' implementation.
+ */
+
+
+
+
+ /**********************************************************************
+ * Includes
+ **********************************************************************/
+#include "motor_cfg.h" /* For this modules definitions */
+
+ /*******************************************************************************************************************************************************************/
+ /* Declaration and Initialization */
+
+
+str_dcm_confige_t ST_g_carMotors[4] =
+{
+
+	{
+		{
+		  .pinMode				=		 GPIO_MODE_GPIO,
+		  .currentLevel			=		 GPIO_CUR_2MA,
+		  .pinDirection			=		 GPIO_DIR_OUTPUT,
+		  .pinLogic				=		 GPIO_LOGIC_LOW,
+		  .portNumber			=		 GPIO_PORTA ,
+		  .pinNumber			=		 GPIO_PIN0
+		},
+		  DCM0_PIN_0
+	},
+
+	{
+		{
+		  .pinMode				=		 GPIO_MODE_GPIO,
+		  .currentLevel			=		 GPIO_CUR_2MA,
+		  .pinDirection			=		 GPIO_DIR_OUTPUT,
+		  .pinLogic				=		 GPIO_LOGIC_LOW,
+		  .portNumber			=		 GPIO_PORTA ,
+		  .pinNumber			=		 GPIO_PIN1
+		},
+		  DCM0_PIN_1
+	},
+
+	{
+		{
+		  .pinMode				=		 GPIO_MODE_GPIO,
+		  .currentLevel			=		 GPIO_CUR_2MA,
+		  .pinDirection			=		 GPIO_DIR_OUTPUT,
+		  .pinLogic				=		 GPIO_LOGIC_LOW,
+		  .portNumber			=		 GPIO_PORTA ,
+		  .pinNumber			=		 GPIO_PIN2
+		},
+		  DCM1_PIN_0
+	},
+
+	{
+		{
+		  .pinMode				=		 GPIO_MODE_GPIO,
+		  .currentLevel			=		 GPIO_CUR_2MA,
+		  .pinDirection			=		 GPIO_DIR_OUTPUT,
+		  .pinLogic				=		 GPIO_LOGIC_LOW,
+		  .portNumber			=		 GPIO_PORTA ,
+		  .pinNumber			=		 GPIO_PIN3
+		},
+		  DCM1_PIN_1
+	}
+};
+
+
+/*******************************************************************************************************************************************************************/
+/****************************************************************************************************************************************/
+
