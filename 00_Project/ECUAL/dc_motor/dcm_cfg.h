2b2818ac59a90212fbba5104fe18079310539c9c
Mahmoud Mowafey <mahmsa37@gmail.com>
2023-06-25 15:38:04 +0300

    adding the configuration header file

--- 00_Project/ECUAL/dc_motor/dcm_cfg.h
+++ 00_Project/ECUAL/dc_motor/dcm_cfg.h
@@ -1,76 +1,50 @@
 /*
- * DCM_Program.c
+ * DCM_cfg.h
  *
  *     Created on: Jun 25, 2023
  *         Author: Mahmoud Mowafey - https://github.com/Mahmoud-Mowafy
- *  Description: This file contains all Direct Current Motor (DCM) functions' implementation.
+ *    Description: This file contains all Direct Current Motor (DCM) functions' prototypes and definitions (Macros) to avoid magic numbers.
  */
 
+#ifndef DCM_CFG_H_
+#define DCM_CFG_H_
 
 
-
- /**********************************************************************
- * Includes
- **********************************************************************/
-#include "motor_cfg.h" /* For this modules definitions */
 
  /*******************************************************************************************************************************************************************/
- /* Declaration and Initialization */
+ /* DCM Includes */
+ /*******************************************************************************************************************************************************************/
 
-
-str_dcm_confige_t ST_g_carMotors[4] =
-{
-
-	{
-		{
-		  .pinMode				=		 GPIO_MODE_GPIO,
-		  .currentLevel			=		 GPIO_CUR_2MA,
-		  .pinDirection			=		 GPIO_DIR_OUTPUT,
-		  .pinLogic				=		 GPIO_LOGIC_LOW,
-		  .portNumber			=		 GPIO_PORTA ,
-		  .pinNumber			=		 GPIO_PIN0
-		},
-		  DCM0_PIN_0
-	},
-
-	{
-		{
-		  .pinMode				=		 GPIO_MODE_GPIO,
-		  .currentLevel			=		 GPIO_CUR_2MA,
-		  .pinDirection			=		 GPIO_DIR_OUTPUT,
-		  .pinLogic				=		 GPIO_LOGIC_LOW,
-		  .portNumber			=		 GPIO_PORTA ,
-		  .pinNumber			=		 GPIO_PIN1
-		},
-		  DCM0_PIN_1
-	},
-
-	{
-		{
-		  .pinMode				=		 GPIO_MODE_GPIO,
-		  .currentLevel			=		 GPIO_CUR_2MA,
-		  .pinDirection			=		 GPIO_DIR_OUTPUT,
-		  .pinLogic				=		 GPIO_LOGIC_LOW,
-		  .portNumber			=		 GPIO_PORTA ,
-		  .pinNumber			=		 GPIO_PIN2
-		},
-		  DCM1_PIN_0
-	},
-
-	{
-		{
-		  .pinMode				=		 GPIO_MODE_GPIO,
-		  .currentLevel			=		 GPIO_CUR_2MA,
-		  .pinDirection			=		 GPIO_DIR_OUTPUT,
-		  .pinLogic				=		 GPIO_LOGIC_LOW,
-		  .portNumber			=		 GPIO_PORTA ,
-		  .pinNumber			=		 GPIO_PIN3
-		},
-		  DCM1_PIN_1
-	}
-};
-
-
+#include "gpio_config.h" /* For this modules definitions */
 /*******************************************************************************************************************************************************************/
-/****************************************************************************************************************************************/
 
+
+
+typedef struct ST_DCM_g_Config_t {
+	Uchar8_t DCM_g_motEnPinNumber0;
+	Uchar8_t DCM_g_motEnPinNumber1;
+	Uchar8_t DCM_g_motEnPortNumber;
+}ST_DCM_g_Config_t;
+
+/**
+ * Defines an enumerated list of the DCM_PINs.
+ * The last element is used to specify the maximum number of
+ * enumerated labels.
+ */
+typedef enum LED_ID {
+	DCM0_PIN_0 = 0,
+	DCM0_PIN_1,
+	DCM1_PIN_0,
+	DCM1_PIN_1
+}enu_dcm_pin_id_t;
+
+/**
+ * Defines the DCM configuration table's elements that are used
+ * by DCM_Init to configure the DC_Motors.
+ */
+typedef struct dcm {
+	ST_dio_pinCfg_t 	 gpio_for_dcm_confige;
+	enu_dcm_pin_id_t	 dcm_pin_id;
+}str_dcm_confige_t;
+
+#endif /* DCM_CFG_H_ */
