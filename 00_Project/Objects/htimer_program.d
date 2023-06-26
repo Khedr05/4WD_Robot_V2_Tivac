239b9ee7a5ac4948e6fac959c8686d5c7f8d954f
SherifKhadr <sherifkhadr1@gmail.com>
2023-06-26 21:26:34 +0300

    SAK : refactor repo

--- 00_Project/Objects/htimer_program.d
+++
@@ -1,4 +0,0 @@
-.\objects\htimer_program.o: ECUAL\h_gpt\htimer_program.c
-.\objects\htimer_program.o: .\COMMON\std_types.h
-.\objects\htimer_program.o: ECUAL\h_gpt\htimer_interface.h
-.\objects\htimer_program.o: .\MCAL\gpt\gpt_interface.h
