6bb0076aa46aaf1e7824e11e6b2a6ac7cf3a7e40
SherifKhadr <sherifkhadr1@gmail.com>
2023-06-28 18:22:19 +0300

    SAK : flowcharts image

--- 00_Project/Objects/main.d
+++ 00_Project/Objects/main.d
@@ -1,20 +1,16 @@
 ./objects/main.o: main.c APPLICATION\app.h COMMON\std_types.h \
   ECUAL\led\led_interface.h COMMON\bit_math.h MCAL\gpio\gpio_interface.h \
-  C:\Users\sharb\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123.h \
-  C:\Users\sharb\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123GH6PM.h \
-  C:\Users\sharb\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\20101\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123.h \
+  C:\Users\20101\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123GH6PM.h \
+  C:\Users\20101\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm4.h \
   C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  C:\Users\sharb\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h \
-  C:\Users\sharb\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h \
-  C:\Users\sharb\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armclang.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
-  C:\Users\sharb\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\mpu_armv7.h \
-  C:\Users\sharb\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\system_TM4C123.h \
+  C:\Users\20101\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\20101\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\20101\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\20101\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
+  C:\Users\20101\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\system_TM4C123.h \
   MCAL\gpio\gpio_config.h MCAL\gpio\gpio_private.h \
   ECUAL\led\led_config.h ECUAL\push_button\push_button_interface.h \
-  COMMON\STD_TYPES.h COMMON\BIT_MATH.h \
   ECUAL\push_button\push_button_cfg.h ECUAL\h_gpt\htimer_interface.h \
   MCAL\gpt\gpt_interface.h ECUAL\pwm\pwm_interface.h ECUAL\pwm\pwm_cfg.h \
-  ECUAL\dc_motor\dcm_interface.h ECUAL\dc_motor\dcm_cfg.h \
-  MCAL\gpio\gpio_config.h
+  ECUAL\dc_motor\dcm_interface.h ECUAL\dc_motor\dcm_cfg.h
