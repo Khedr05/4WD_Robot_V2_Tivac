689c42dc4ac247853f403148b08b5db810934e29
Sharpel Malak <Sharbelmalak@gmail.com>
2023-07-02 21:44:17 +0300

    deleting old project

--- 00_Project/Objects/dcm_cfg.d
+++
@@ -1,2 +0,0 @@
-./objects/dcm_cfg.o: ECUAL\dc_motor\dcm_cfg.c ECUAL\dc_motor\dcm_cfg.h \
-  COMMON\std_types.h MCAL\gpio\gpio_config.h
--- 00_Project/Objects/htimer_program.d
+++
@@ -1,3 +0,0 @@
-./objects/htimer_program.o: ECUAL\h_gpt\htimer_program.c \
-  COMMON\std_types.h ECUAL\h_gpt\htimer_interface.h \
-  MCAL\gpt\gpt_interface.h
